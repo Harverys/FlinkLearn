38e902314ec286aab4b624b22f933c42f47e8448
hw <714139391@qq.com>
2022-03-20 14:28:58 +0800

    【2022-03-20】 首次提交Flink批处理文件

---
+++ src/main/java/com/sinovatio/Flink/BatchWordCount.java
@@ -0,0 +1,17 @@
+package com.sinovatio.Flink;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+/**
+ * Description: BatchWordCount
+ *
+ * @author hw
+ * @version V1.0
+ * @date 2022-03-20
+ */
+public class BatchWordCount {
+    public static void main(String[] args) {
+        ExecutionEnvironment executionEnvironment = ExecutionEnvironment.getExecutionEnvironment();
+
+    }
+}
